63f00c8b3d763d9e440d8ceac240a4fd47488ef2
OffensiveExile <OffensiveExile@users.noreply.github.com>
2017-03-02 10:12:17 +0000

    Version 0.2, utilized step for loop.

--- OSRS_Pinger.bat
+++ OSRS_Pinger.bat
@@ -2,9 +2,9 @@
 REM Originally created by /u/Cameronza. Updated by /u/OffensiveExile.
 
 ECHO OldSchool Runescape - World Pinger!
+ECHO Version 0.2 - Credits to /u/Cameronza.
 ECHO.
 SETLOCAL enabledelayedexpansion
-SET osworld=1,2,3,4,5,6,8,9,10,11,12,13,14,16,17,18,19,20,21,22,25,26,27,28,29,30,33,34,35,36,37,38,41,42,43,44,45,46,49,50,51,52,53,54,57,58,59,60,61,62,65,66,67,68,69,70,73,74,75,76,77,78,81,82,83,84,85,86,93,94
 SET preword=300
 ECHO A percentage of the world ping is added to the world ping to find more worlds.
 ECHO Type 0 if you want to find the absolute lowest world ping possible.
@@ -12,14 +12,13 @@ ECHO Type 9999 if you want to find all words.
 SET /p percentage="Percentage: "
 ECHO.
 ECHO If no worlds show, please wait until instructed otherwise.
-FOR %%i IN (%osworld%) DO (
-	FOR /f "tokens=4 delims=Replyfrombytes=time<ms" %%x in ('Ping oldschool%%i.runescape.com -n 1 ^| find "TTL" ') DO (
+FOR /L %%i IN ( 1,1,99) DO (
+	FOR /f "tokens=4 delims=Replyfrombytes=time<ms" %%x in ('Ping oldschool%%i.runescape.com -w 1000 -n 1 ^| find "TTL" ') DO (
 		set /a world=preword+%%i
 		IF !osping! GEQ %%x (
 			ECHO WORLD_!world! = %%x ms
 			SET /a osping = %%x*percentage
 			SET /a osping = !osping:~0,-2!+%%x
-
 		) ELSE IF %%i==1 (
 			SET /a osping = %%x*percentage
 			SET /a osping = !osping:~0,-2!+%%x
